0194d63b335e665ac8960611a9806f00998c3093
Walt Pach <officialyoutextcontrol@gmail.com>
2015-02-28 16:25:47 -0500

    Working on text rendering.

--- src/net/BiggerOnTheInside/Binder/GameLoop.java
+++ src/net/BiggerOnTheInside/Binder/GameLoop.java
@@ -52,6 +52,10 @@ public class GameLoop {
 			c.d();
 		}
 		
+		Font font = new Font("Times New Roman", Font.BOLD, 24);
+		
+		_font = new TrueTypeFont(font, false);
+		
 		Mouse.setGrabbed(true);
 		while(!Display.isCloseRequested() && !Keyboard.isKeyDown(Globals.EXIT_KEY)){
 			render();
@@ -101,11 +105,15 @@ public class GameLoop {
         glLoadIdentity();
 	}*/
 
+	TrueTypeFont _font;
 	
 	public void render3D(){
 		/* For some reason, needs commenting for camera to work :p */
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT); // Clear The Screen And The Depth Buffer
-	    
+	    setOrthoOn();
+	    _font.drawString(100, 25, "Hello world!", Color.red);
+		setOrthoOff();
+		
 		BlockRenderer.renderBlock(Block.DIRT, 0f, 0f, 0f);
 		BlockRenderer.renderWireframeBlock(Block.DIRT, 0f, 1f, 1f);
 	}
@@ -113,5 +121,28 @@ public class GameLoop {
 	
 	public static Player getPlayerObject(){
 		return p;
+	}
+	
+	public static void setOrthoOn()
+	{
+	        // prepare to render in 2D
+	        GL11.glDisable(GL11.GL_DEPTH_TEST);             // so 2D stuff stays on top of 3D scene
+	        GL11.glMatrixMode(GL11.GL_PROJECTION);
+	        GL11.glPushMatrix();                            // preserve perspective view
+	        GL11.glLoadIdentity();                          // clear the perspective matrix
+	        GL11.glOrtho(0, Binder.getDisplayMode().getWidth(), Binder.getDisplayMode().getHeight(), 0,-1,1);  // turn on 2D
+	        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+	        GL11.glPushMatrix();                // Preserve the Modelview Matrix
+	        GL11.glLoadIdentity();              // clear the Modelview Matrix
+	}
+	 
+	public static void setOrthoOff()
+	{
+	        // restore the original positions and views
+	        GL11.glMatrixMode(GL11.GL_PROJECTION);
+	        GL11.glPopMatrix();
+	        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+	        GL11.glPopMatrix();
+	        GL11.glEnable(GL11.GL_DEPTH_TEST);      // turn Depth Testing back on
 	}
 }
